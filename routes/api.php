c948f640d315481ceda20927c42fdbe0b3053efa
danila <danilapayne@gmail.com>
2025-12-17 10:28:13 +0500

    added prefix

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/me', [UserController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/auth/me', [UserController::class, 'me']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
